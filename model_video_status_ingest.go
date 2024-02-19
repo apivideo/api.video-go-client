8177e69a43db182f6ddcefa37cc6e8c472d1b0ac
olivierapivideo <olivierapivideo@users.noreply.github.com>
2024-02-19 09:52:22 +0000

    Update VideoStatusIngest enum descriptions

--- model_video_status_ingest.go
+++ model_video_status_ingest.go
@@ -16,7 +16,7 @@ import (
 
 // VideoStatusIngest Details about the capturing, transferring, and storing of your video for use immediately or in the future.
 type VideoStatusIngest struct {
-	// There are three possible ingest statuses. missing - you are missing information required to ingest the video. uploading - the video is in the process of being uploaded. uploaded - the video is ready for use.
+	// There are four possible statuses depending on how you provide a video file: - `uploading` - the API is gathering the video source file from an upload. - `uploaded` - the video file is fully uploaded. - `ingesting` - the API is gathering the video source file from either a URL, or from cloning. - `ingested` - the video file is fully stored.
 	Status *string `json:"status,omitempty"`
 	// The size of your file in bytes.
 	Filesize *NullableInt32 `json:"filesize,omitempty"`
